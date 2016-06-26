acfde23eb209e5d78df79a2ba083ddd3d68e15f0
Pavel Kizernis <pashakiz@gmail.com>
2016-06-27 00:48:14 +0300

    map working!

--- js/common.js
+++ js/common.js
@@ -72,3 +72,12 @@ $(document).ready(function() {
 	});
 
 });
+
+jQuery(window).load(function() {
+	var svgobject = $('#svgmap');
+	if ('contentDocument' in svgobject) {              // У нас действительно там что-то есть?
+		var svgdom = jQuery(svgobject.contentDocument);  // Получаем доступ к объектной модели SVG-файла
+		// Теперь делаем свою работу, например:
+		jQuery("#map", svgdom).attr("fill", "red");  // Находим тег с id="figure1" в SVG DOM и заливаем его красным
+	}
+});
